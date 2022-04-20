aa223b120aab91f0ec387a3ea3afcedb54440c71
유재민 <yoojamer@gmail.com>
2022-04-21 02:43:35 +0900

    Tab 작업 완료

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tab.scss";
 
 const Tab = () => {
-  return <div id="Tab">Tab</div>;
+  const [btnActive, setBtnActive] = useState([true, false, false]);
+
+  const vegetableNameHandler = (id) => {
+    const activeCheck = btnActive.map((el, index) => {
+      return index === id - 1;
+    });
+
+    setBtnActive(activeCheck);
+  };
+
+  return (
+    <section id="Tab">
+      <h1 className="title">2. Tab</h1>
+      <div className="container">
+        {VEGETABLE_LIST.map((elem, index) => (
+          <button
+            className={btnActive[index] ? "btnActive" : "btnUnActive"}
+            key={elem.id}
+            onClick={() => vegetableNameHandler(elem.id)}
+          >
+            {elem.name}
+          </button>
+        ))}
+      </div>
+      <div className="sliderLineWrap">
+        <div
+          className="sliderLine"
+          style={{
+            transform: `translateX(${btnActive.indexOf(true) * 100 + "%"})`,
+          }}
+        ></div>
+      </div>
+    </section>
+  );
 };
 
 export default Tab;
+
+const VEGETABLE_LIST = [
+  { id: 1, name: "감자" },
+  { id: 2, name: "고구마" },
+  { id: 3, name: "카레라이스" },
+];
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,28 +1,27 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Toggle.scss";
 
 const Toggle = () => {
-  const [btnHandler, setbtnHandler] = useState(true);
+  const [btnActive, setbtnActive] = useState(true);
 
-  const btnDisplayHandler = (event) => {
-    setbtnHandler(event.target.innerHTML === "기본");
+  const btnHandler = (event) => {
+    setbtnActive(event.target.innerHTML === "기본");
   };
 
   return (
     <section id="Toggle">
       <h1 className="title">1. Toggle</h1>
       <div className="container">
-        <div className={btnHandler ? "btnBgDefault" : "btnBgDetail"}></div>
+        <div className={btnActive ? "btnBgDefault" : "btnBgDetail"}></div>
         <button
-          className={btnHandler ? "actionBtnDetail" : "actionBtnDefault"}
-          onClick={btnDisplayHandler}
+          className={btnActive ? "actionBtnDetail" : "actionBtnDefault"}
+          onClick={btnHandler}
         >
           기본
         </button>
         <button
-          className={btnHandler ? "actionBtnDefault" : "actionBtnDetail"}
-          onClick={btnDisplayHandler}
+          className={btnActive ? "actionBtnDefault" : "actionBtnDetail"}
+          onClick={btnHandler}
         >
           상세
         </button>
